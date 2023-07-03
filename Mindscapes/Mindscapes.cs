e444a551bfd99d2fdcdac8a62fbce679164c816e
coderCleric <wescamphouse@gmail.com>
2023-07-03 10:35:08 -0400

    Add project files.

---
+++ Mindscapes/Mindscapes.cs
@@ -0,0 +1,25 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace Mindscapes
+{
+    public class Mindscapes : ModBehaviour
+    {
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(Mindscapes)} is loaded!", MessageType.Success);
+
+            // Get the New Horizons API and load configs
+            var newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            newHorizons.LoadConfigs(this);
+
+            // Example of accessing game code.
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            {
+                if (loadScene != OWScene.SolarSystem) return;
+                ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+            };
+        }
+    }
+}
