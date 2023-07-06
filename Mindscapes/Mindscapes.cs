2e06fefeba05c1c3055ab1cb69b2d5f7ebeea837
coderCleric <wescamphouse@gmail.com>
2023-07-06 15:08:18 -0400

    Added the basic pickup

--- Mindscapes/Mindscapes.cs
+++ Mindscapes/Mindscapes.cs
@@ -1,25 +1,52 @@
 ﻿using OWML.Common;
 using OWML.ModHelper;
+using UnityEngine.Events;
+using UnityEngine;
+using Mindscapes.miscbehaviours;
 
 namespace Mindscapes
 {
     public class Mindscapes : ModBehaviour
     {
+        private static Mindscapes instance;
+
+        /**
+         * Need to do some prep work on start
+         */
         private void Start()
         {
-            // Starting here, you'll have access to OWML's mod helper.
-            ModHelper.Console.WriteLine($"My mod {nameof(Mindscapes)} is loaded!", MessageType.Success);
+            // Say that it worked
+            ModHelper.Console.WriteLine($"{nameof(Mindscapes)} is loaded!", MessageType.Success);
+
+            instance = this;
 
             // Get the New Horizons API and load configs
-            var newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            INewHorizons newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
             newHorizons.LoadConfigs(this);
 
-            // Example of accessing game code.
-            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            //Do stuff when the system finishes loading
+            UnityEvent<string> loadCompleteEvent = newHorizons.GetStarSystemLoadedEvent();
+            loadCompleteEvent.AddListener(PrepSystem);
+        }
+
+        /**
+         * Do some things when the system loads, mostly prepping more complicated objects
+         */
+        private void PrepSystem(string s)
+        {
+            if(s.Equals("SolarSystem")) //Only do things in the base-game system
             {
-                if (loadScene != OWScene.SolarSystem) return;
-                ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
-            };
+                //Put the necessary component on the device
+                GameObject.Find("TimberHearth_Body/Sector_TH/mind_exhibit/mind_device").AddComponent<MindDevice>();
+            }
+        }
+
+        /**
+         * A simple debug print
+         */
+        public static void DebugPrint(string msg)
+        {
+            instance.ModHelper.Console.WriteLine(msg);
         }
     }
 }
---
+++ Mindscapes/miscbehaviours/MindDevice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Mindscapes.miscbehaviours
+{
+    internal class MindDevice : OWItem
+    {
+        /**
+         * Gives the display name
+         */
+        public override string GetDisplayName()
+        {
+            return "Nomai Device";
+        }
+
+        /**
+         * Activates the mind invasion volumes when the item is picked up
+         */
+        public override void PickUpItem(Transform holdTranform)
+        {
+            base.PickUpItem(holdTranform);
+        }
+
+        /**
+         * Deactivates the mind invasion volumes when the item is put down
+         */
+        public override void DropItem(Vector3 position, Vector3 normal, Transform parent, Sector sector, IItemDropTarget customDropTarget)
+        {
+            base.DropItem(position, normal, parent, sector, customDropTarget);
+        }
+    }
+}
